315c2b191e30f58728049e5db37344b914a7e94b
Mouhamed Amine BOUCHIHA <49335275+mohaminemed@users.noreply.github.com>
2025-09-18 16:54:45 +0200

    ABC-DFL

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.0",  
+  solidity: "0.8.0",  // Solidaty compiler 
   networks: {
     hardhat: {
       chainId: 1337,
